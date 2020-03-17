4ac9bee0ed8cf7d3c9d5c2dadf2b4d12720f2daf
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-03-17 20:44:07 +0300

    add animations

--- app/src/main/java/com/example/catsvsdogs/FragmentCats.kt
+++ app/src/main/java/com/example/catsvsdogs/FragmentCats.kt
@@ -1,12 +1,13 @@
 package com.example.catsvsdogs
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_fragment_cats.*
-import kotlinx.android.synthetic.main.activity_fragment_dogs.*
+
 
 class FragmentCats : Fragment() {
 
@@ -15,8 +16,10 @@ class FragmentCats : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.activity_fragment_cats, container, false)
     }
+
+    private lateinit var rocketAnimation: AnimationDrawable
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -26,7 +29,13 @@ class FragmentCats : Fragment() {
         clickCats.setOnClickListener {
             DataStorage.instance.player1Points += 1
             textCats.text = DataStorage.instance.player1Name
+
+
+            val rocketImage = animationsCats.apply {
+                setBackgroundResource(R.drawable.catcat)
+                rocketAnimation = background as AnimationDrawable
+            }
+            rocketAnimation.start()
         }
     }
-
 }
